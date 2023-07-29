3234fa2663d10f1c2ad8c0afc78c38929deb8abb
Yachtzee <edwinalban@gmail.com>
2023-07-29 13:27:12 -0700

    added relational mapping for Product/Category/Tag models, added routes for GET/POST/PUT/DELETE for category routes

--- models/index.js
+++ models/index.js
@@ -5,12 +5,30 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE'
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag
+  },
+  as: 'tags',
+  foreignKey: 'tag_id'
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag
+  },
+  as: 'products',
+  foreignKey: 'product_id' 
+});
 
 module.exports = {
   Product,
