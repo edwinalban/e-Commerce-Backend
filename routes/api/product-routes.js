0f4dcdd23aea70af8ce88eb0af8f6f4df1bc0f05
Yachtzee <edwinalban@gmail.com>
2023-07-29 20:44:22 -0700

    removed cruft

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -111,14 +111,14 @@ router.put('/:id', (req, res) => {
 router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
   try {
-    const deleteProduct = await Product.destroy({
+    await Product.destroy({
       where: {
         id: req.params.id
       }
 })
-res.status(200).json(deleteProduct);
+res.status(200).json({ message: `Product Deleted!` });
 } catch (e) {
-  res.status(500).json({message: `Error deleting ${deleteProduct}`});
+  res.status(500).json({message: `Error deleting Product`});
 }
 });
 
